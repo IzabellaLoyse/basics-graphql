3f6f012ea1d4a7b0cc51f816cf86d725bbce2f28
Izabella Loyse <izabellaloyse13@gmail.com>
2023-02-03 16:55:06 -0300

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,37 @@
+import { gql, useQuery } from '@apollo/client';
+import './App.css';
+import Form from './components/Form/Form';
+
+type User = {
+  id: string;
+  name: string;
+};
+
+export const GET_USER = gql`
+  query {
+    users {
+      id
+      name
+    }
+  }
+`;
+
+function App() {
+  const { data, loading } = useQuery<{ users: User[] }>(GET_USER);
+  return (
+    <div className="App">
+      {loading && <p>Carregando...</p>}
+      <div>
+        <Form />
+      </div>
+
+      <ul>
+        {data?.users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Form/Form.tsx
@@ -0,0 +1,43 @@
+import { gql, useMutation } from '@apollo/client';
+import { FormEvent, useState } from 'react';
+import { GET_USER } from '../../App';
+
+const CREATE_USER = gql`
+  mutation ($name: String!) {
+    createUser(name: $name) {
+      id
+      name
+    }
+  }
+`;
+
+function Form() {
+  const [name, setName] = useState();
+  const [createUser, { data, loading, error }] = useMutation(CREATE_USER);
+
+  const handleCreateUser = async (event: FormEvent) => {
+    event.preventDefault();
+
+    if (!name) return;
+
+    await createUser({
+      variables: {
+        name,
+      },
+      refetchQueries: [GET_USER],
+    });
+  };
+
+  return (
+    <form onSubmit={handleCreateUser}>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value as any)}
+      />
+      <button type="submit">Enviar</button>
+    </form>
+  );
+}
+
+export default Form;
